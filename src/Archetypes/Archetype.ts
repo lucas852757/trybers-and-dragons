5602392fc0b1a18f1e7100f9148e071899cbd3a8
lucas852757 <lucasaparicio11@gmail.com>
2022-09-02 20:43:41 +0000

    criado src/Archetypes/Archetype.ts

--- src/Archetypes/Archetype.ts
+++ src/Archetypes/Archetype.ts
@@ -1,9 +1,9 @@
 import { EnergyType } from '../TypeEnergyType';
 
 abstract class Archetype {
-  private _name: string;
-  private _special: number;
-  private _cost: number;
+  protected _name: string;
+  protected _special: number;
+  protected _cost: number;
 
   constructor(name: string, special = 0, cost = 0) {
     this._name = name;
