561caaa97570c33beb2b86c0ad25a4e7776ba89e
lucas852757 <lucasaparicio11@gmail.com>
2022-09-02 20:58:42 +0000

    criado src/Archetypes/Necromancer.ts

---
+++ src/Archetypes/Necromancer.ts
@@ -0,0 +1,26 @@
+import { EnergyType } from '../TypeEnergyType';
+import Archetype from './Archetype';
+
+class Necromancer extends Archetype {
+  static lastId = 0;
+  private _energyType: 'mana' = 'mana';
+  constructor(name: string) {
+    super(name);
+    this._name = name;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  static createdArchetypeInstances():number {
+    this.lastId += 1;
+    return this.lastId;
+  }
+}
+
+export default Necromancer;
