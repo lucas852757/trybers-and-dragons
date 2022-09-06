f8d97c229bd12c5207fa305de62031824ad8c1ea
lucas852757 <lucasaparicio11@gmail.com>
2022-09-06 17:04:06 +0000

    corrigido bug

--- src/Battle/PVE.ts
+++ src/Battle/PVE.ts
@@ -1,19 +1,17 @@
-import Character from '../Character';
-import Fighter from '../Fighter';
-import Monster from '../Monster';
+import Fighter, { SimpleFighter } from '../Fighter';
 import Battle from './Battle';
 
 class PVE extends Battle {
-  private person: Character | Fighter;
-  private monsters: [];
-  constructor(person: Character | Fighter, monsters: []) {
+  private person: Fighter;
+  private monsters: SimpleFighter[];
+  constructor(person: Fighter, monsters: SimpleFighter[]) {
     super(person);
     this.person = person;
     this.monsters = monsters;
   }
 
   fight():number {
-    this.monsters.forEach((monster: Monster) => {
+    this.monsters.forEach((monster) => {
       while (this.person.lifePoints > 0 && monster.lifePoints > 0) {
         this.person.attack(monster);
         monster.attack(this.person);
