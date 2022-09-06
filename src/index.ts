e2e5ae92b54a0150fb2c5273bbbfa9b0d7154dd5
lucas852757 <lucasaparicio11@gmail.com>
2022-09-06 18:53:43 +0000

    bug

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-import Character from './Character';
+// import Character from './Character';
 
-const player1: Character = {};
-const player2: Character = {};
-const player3: Character = {};
+// const player1: Character = {};
+// const player2: Character = {};
+// const player3: Character = {};
 
-player1.levelUp();
-player1.levelUp();
+// player1.levelUp();
+// player1.levelUp();
+
+// export {player1, player2, player3 };
