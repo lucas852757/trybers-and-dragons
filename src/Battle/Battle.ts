aecdcba3a009d052a748cde2f0b9b6dd9a7ae7f3
lucas852757 <lucasaparicio11@gmail.com>
2022-09-06 02:44:33 +0000

    restaurado arquivo original

--- src/Battle/Battle.ts
+++ src/Battle/Battle.ts
@@ -1,11 +1,11 @@
 import Fighter from '../Fighter';
 
 abstract class Battle {
-  constructor(protected player1: Fighter, protected player2: Fighter) { }
+  constructor(protected player: Fighter) { }
 
   // Should return 1 if player wins, -1 otherwise
   fight(): number {
-    return this.player1.lifePoints === -1 ? -1 : 1;
+    return this.player.lifePoints === -1 ? -1 : 1;
   }
 }
 
