453cf124ab18f220c25951d85b9e87428285759b
lucas852757 <lucasaparicio11@gmail.com>
2022-09-06 03:11:33 +0000

    feito alteracoes

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -1,8 +1,23 @@
 // import Fighter from '../Fighter';
+import Character from '../Character';
+import Fighter from '../Fighter';
 import Battle from './Battle';
 
 class PVP extends Battle {
+  player1: Character | Fighter;
+  player2: Character | Fighter;
+  constructor(player1: Character | Fighter, player2: Character | Fighter) {
+    super(player1);
+    this.player1 = player1;
+    this.player2 = player2;
+  }
+
   fight(): number {
+    while (this.player1.lifePoints > 0 && this.player2.lifePoints > 0) {
+      this.player1.attack(this.player2);
+      this.player2.attack(this.player1);
+    }
+    
     return super.fight();
   }
 }
